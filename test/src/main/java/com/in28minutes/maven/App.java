b1489c65865c13367a07032cece92956c70eaf42
Sathya Raghavendra <41712465+sathyarms@users.noreply.github.com>
2019-09-17 12:51:03 +0530

    udpate java file

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World  Test Test Test" );
+	System.out.println( "Hello World  Test Test Test Test1" );
     }
 
 	public int calculateSomething() {
